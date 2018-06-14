d81facd8ef384b47035a79eaa8a13c384c24b027
unknown <1169470657@qq.com>
2018-06-14 21:04:48 +0800

    '6.14'

--- egg-example/app/controller/home.js
+++ egg-example/app/controller/home.js
@@ -30,38 +30,48 @@ class HomeController extends Controller {
         "label": "年龄",
       }],
     }
-    const Formdata = ctx.model.Formdata;
-    const formdata = new Formdata({
-      name: data.name,
-      token: data.token,
-      fields: data.fields
-    });
-    formdata.save();
+    // const Formdata = ctx.model.Formdata;
+    // const formdata = new Formdata({
+    //   name: data.name,
+    //   token: data.token,
+    //   fields: data.fields
+    // });
+    // formdata.save();
     ctx.body = 'hi, egg';
   }
   async adduserdata() {
     const ctx = this.ctx;
-    const data = {
-      "name": "未命名表单5",
-      "token": "1vylfY",
-      "fields": {
-        "姓名": "施心平",
-        "原因": "123123123123123123123123",
-        "性别": "男",
-        "年龄": "3",
-      }
-    }
+    // const data = {
+    //   "name": "未命名表单5",
+    //   "token": "1vylfY",
+    //   "fields": {
+    //     "姓名": "施心平",
+    //     "原因": "123123123123123123123123",
+    //     "性别": "男",
+    //     "年龄": "3",
+    //   }
+    // }
 
-    for (let i = 0; i < 100; i++) {
-      const Userdata = ctx.model.Userdata;
-      const userdata = new Userdata({
-        name: data.name,
-        token: data.token,
-        fields: data.fields
-      });
-      userdata.save();
-    }
+    // for (let i = 0; i < 100; i++) {
+    //   const Userdata = ctx.model.Userdata;
+    //   const userdata = new Userdata({
+    //     name: data.name,
+    //     token: data.token,
+    //     fields: data.fields
+    //   });
+    //   userdata.save();
+    // }
     ctx.body = 'ok';
+  }
+  async getTableList() {
+    const ctx = this.ctx;
+    const Formdata = ctx.model.Formdata;
+    const tableList = await new Promise((resolve,reject) => {
+      Formdata.find({},(err,docs) => {
+        resolve(docs);
+      })
+    })
+    ctx.body = tableList;
   }
 }
 
--- egg-example/app/router.js
+++ egg-example/app/router.js
@@ -7,4 +7,5 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.get('/adduserdata', controller.home.adduserdata);
+  router.get('/getTableList', controller.home.getTableList);
 };
